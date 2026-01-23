b53486e72fc99a5952f2f00215002351e9e2f8c3
Maryam Kifaaya <kifaaya815@gmail.com>
2026-01-23 15:05:19 +0530

    second commit

--- TriangleShape.py
+++ TriangleShape.py
@@ -2,6 +2,12 @@ from manim import *
 
 class TriangleShape(Scene):
     def construct(self):
-        triangle = Triangle(color=PINK).scale(2)
+        triangle = Triangle(color=PINK,
+                            fill_color=BLUE,
+                            fill_opacity=1
+                            ).scale(2)
+        
+        triangle.set_stroke(width=50)
         self.add(triangle)
-       
+    
+        
