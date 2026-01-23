397de8603fd7a51eb28dbd36480030f9355ca853
Maryam Kifaaya <kifaaya815@gmail.com>
2026-01-23 15:15:20 +0530

    second commit

--- manim_shapes.py
+++ manim_shapes.py
@@ -2,10 +2,15 @@ from manim import *
 
 class ShapeBuild(Scene):
     def construct(self):
-        circle=Circle(radius=1,color=PINK)
-        square=Square(side_length=2,color=BLUE)
-        triangle=Triangle(color=YELLOW)
+        circle=Circle(radius=1,color=PINK,fill_color=BLUE,fill_opacity=1)
+        square=Square(side_length=2,color=BLUE,fill_color=PINK,fill_opacity=1)
+        triangle=Triangle(color=YELLOW,fill_color=GREEN,fill_opacity=1)
         dot=Dot(point=UP*2,color=GREEN)
+
+        circle.set_stroke(width=30)
+        square.set_stroke(width=20)
+        triangle.set_stroke(width=40)
+        dot.set_stroke(width=70)
 
         square.next_to(circle,RIGHT)
         triangle.next_to(circle,LEFT)
